c52f55a5f458a01cedf880201296643f76a97109
jasha.chec <jasha@chec.de>
2024-02-19 19:22:49 +0100

    feat(page): import module

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
-import Image from "next/image";
 import MainStage from "./components/Modules/Stage/MainStage";
+import ConferenceStandings from "./Components/Modules/ConferenceStandings/ConferenceStandings";
 import styles from "./page.module.scss";
 
 export default function Home() {
   return (
     <div>
       <MainStage />
+      <section className={styles.section}>
+        <ConferenceStandings headline="Eastern Conference" conference="east" />
+        <ConferenceStandings headline="Western Conference" conference="west" />
+      </section>
     </div>
   );
 }
