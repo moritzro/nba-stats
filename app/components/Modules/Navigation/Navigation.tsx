c03d5926e0f5a03533236a9ae3057b1a7d89db9b
moritzro <97282836+moritzro@users.noreply.github.com>
2024-02-20 20:22:53 +0100

    feat(Navigation.tsx): created Navigation

---
+++ app/components/Modules/Navigation/Navigation.tsx
@@ -0,0 +1,29 @@
+import { FC } from "react";
+import Link from "next/link";
+import styles from "./Navigation.module.scss";
+import { link } from "fs";
+
+interface NavigationProps {
+  navLinkName: Array<string>;
+  navLink: Array<string>;
+}
+
+const Navigation: FC<NavigationProps> = ({ navLinkName, navLink }) => {
+  return (
+    <div className={styles.navWrapper}>
+      <div className={styles.navLogo}>NBA Stats</div>
+      <nav>
+        <ul>
+          {navLink.length > 0 &&
+            navLink.map((link, index) => (
+              <li>
+                <Link href={link}>{navLinkName[index]}</Link>
+              </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navigation;
