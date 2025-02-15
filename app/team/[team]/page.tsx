4a0d342edc64a7cb3e498b8d78a0df99dbcd9c67
Moritz Rohde <97282836+moritzro@users.noreply.github.com>
2025-02-15 08:24:38 +0100

    feat(team page): added new upcoming games module

--- app/team/[team]/page.tsx
+++ app/team/[team]/page.tsx
@@ -1,7 +1,14 @@
 import StageTeamDetails from "@/app/components/modules/StageTeamDetails/StageTeamDetails";
+import TeamUpcomingGames from "@/app/components/modules/UpcomingGames/UpcomingGames";
 
-const Team = ({ params: { team } }) => {
-  return <StageTeamDetails id={team} />;
+const Team = async ({ params }: { params: Promise<{ team: number }> }) => {
+  const teamId = (await params).team;
+  return (
+    <>
+      <StageTeamDetails id={teamId} />
+      <TeamUpcomingGames id={teamId} />
+    </>
+  );
 };
 
 export default Team;
